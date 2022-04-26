e80f639bb34e372f9326c5e4ffd0d981ee90107d
Terry Kuo <terrykuo501@gmail.com>
2022-04-26 16:54:07 +0800

    Initial Commit

---
+++ FivestarKeyBoardAvoidanceDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FivestarKeyBoardAvoidanceDemo
+//
+//  Created by Terry Kuo on 2022/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FivestarKeyBoardAvoidanceDemo/FivestarKeyBoardAvoidanceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FivestarKeyBoardAvoidanceDemoApp.swift
+//  FivestarKeyBoardAvoidanceDemo
+//
+//  Created by Terry Kuo on 2022/4/26.
+//
+
+import SwiftUI
+
+@main
+struct FivestarKeyBoardAvoidanceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
